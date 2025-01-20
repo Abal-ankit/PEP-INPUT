32f7458d80fce09aebf08789a6dc43bd3024bafd
Ankit Raj <ankitraj90747@gmail.com>
2025-01-20 11:42:39 +0530

    Class Component, Functional Component and Arrow Function Component

--- Day05/lpuapp/src/components/Header.js
+++ Day05/lpuapp/src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './style.css';
+// function Header() {
+//   return (
+//     <div>This is my first React app.</div>
+//   )
+// }
 
-function Header() {
-  return (
-    <div>This is my first React app.</div>
-  )
+// export default Header;
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <p className='para'>Header Component</p>
+      </>
+    )
+  }
 }
 
 export default Header;
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Day04 => Arrow Function, Object Literal Enhacement and coomputed Property.
 # Day05 => create-react-app and react application folder structure and workflow.
+# Day06 => Class Component, Functional Component and Arrow Function Component.
